7fb7577efb1453004e206851ab9073177b2f9ef1
Amlan Deep Mohapatra <amlandeep09012003@gmail.com>
2024-07-04 02:04:39 +0530

    commit 2

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,7 +1,24 @@
 const express = require('express'); 
+const mongoose = require('mongoose');
+require("dotenv").config();
 const app = express();
 const port = 8000;
+const connectionString = "mongodb+srv://amlan:" +
+    process.env.MONGO_PASSWORD +
+    "@cluster0.a7jrkd4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+
+mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDB", err);
+    })
+    ;
 //get API
 app.get("/", (req, res) => {
     //req contains data for all request
