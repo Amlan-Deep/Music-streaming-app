b8f816921da21f55e827941ddd08208666c17db6
Amlan Deep Mohapatra <amlandeep09012003@gmail.com>
2024-07-04 15:14:33 +0530

    3rd commit

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const mongoose = require('mongoose');
 require("dotenv").config();
+const User = require("./models/User");
+const passport = require('passport');
 const app = express();
 const port = 8000;
 const connectionString = "mongodb+srv://amlan:" +
@@ -19,6 +21,27 @@ mongoose.connect(connectionString, {
         console.log("Error connecting to MongoDB", err);
     })
     ;
+
+//setup passport-jwt
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "asdfghjhgfd";
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+
 //get API
 app.get("/", (req, res) => {
     //req contains data for all request
