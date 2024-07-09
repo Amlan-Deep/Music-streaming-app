8360d93b04df54888db6ab82c90dc34072077cab
Amlan Deep Mohapatra <amlandeep09012003@gmail.com>
2024-07-09 12:36:51 +0530

    9th july commit

--- spotify-frontend/src/routes/Login.js
+++ spotify-frontend/src/routes/Login.js
@@ -1,35 +1,9 @@
-import {useState} from "react";
 import {Icon} from "@iconify/react";
 import TextInput from "../components/shared/TextInput";
 import PasswordInput from "../components/shared/PasswordInput";
-import {Link, useNavigate} from "react-router-dom";
-import {makeUnauthenticatedPOSTRequest} from "../utils/serverHelpers";
-import {useCookies} from "react-cookie";
+import { Link } from "react-router-dom";
 
 const LoginComponent = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [cookies, setCookie] = useCookies(["token"]);
-    const navigate = useNavigate();
-
-    const login = async () => {
-        const data = {email, password};
-        const response = await makeUnauthenticatedPOSTRequest(
-            "/auth/login",
-            data
-        );
-        if (response && !response.err) {
-            const token = response.token;
-            const date = new Date();
-            date.setDate(date.getDate() + 30);
-            setCookie("token", token, {path: "/", expires: date});
-            alert("Success");
-            navigate("/home");
-        } else {
-            alert("Failure");
-        }
-    };
-
     return (
         <div className="w-full h-full flex flex-col items-center">
             <div className="logo p-5 border-b border-solid border-gray-300 w-full flex justify-center">
@@ -41,26 +15,17 @@ const LoginComponent = () => {
                     To continue, log in to Spotify.
                 </div>
                 <TextInput
-                    label="Email address or username"
-                    placeholder="Email address or username"
+                    label="Email address or Username"
+                    placeholder="Email address or Username"
                     className="my-6"
-                    value={email}
-                    setValue={setEmail}
                 />
-                <PasswordInput
+                <PasswordInput 
                     label="Password"
                     placeholder="Password"
-                    value={password}
-                    setValue={setPassword}
                 />
                 <div className=" w-full flex items-center justify-end my-8">
                     <button
-                        className="bg-green-400 font-semibold p-3 px-10 rounded-full"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            login();
-                        }}
-                    >
+                        className="bg-green-400 font-semibold p-3 px-10 rounded-full">
                         LOG IN
                     </button>
                 </div>
@@ -74,6 +39,6 @@ const LoginComponent = () => {
             </div>
         </div>
     );
-};
+}
 
 export default LoginComponent;
